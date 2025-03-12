d94c3a4e4b2c536a8ffdb9c2bd70c5d2dcf6fa9b
EngineerIDE <doc@engineeride.com>
2025-03-12 05:59:40 -0500

    Deleted Exercise 2-1 file

--- Examples/exercise_02_01.py
+++
@@ -1,8 +0,0 @@
-# exercise_02_01.py, Chapter 02, Python Crash Course
-# store a message in a variable, and then print that
-# message.
-
-secret_message = "This is a message."
-print(secret_message)
-
-
