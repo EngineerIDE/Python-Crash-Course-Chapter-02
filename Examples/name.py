f7d9b01f53be965c9b5d8892e2325a222ad57b49
EngineerIDE <doc@engineeride.com>
2025-03-12 05:57:48 -0500

    Added name.py file

---
+++ Examples/name.py
@@ -0,0 +1,8 @@
+# name.py, Chapter 02, Python Crash Course
+# program prints "Ada Lovelace"
+
+name = "ada lovelace"
+print(name.title()) # name is the variable
+                    # title() is the method
+
+
